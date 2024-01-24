8f2c314843c0575878016ac34acac94238dabadb
Omar Gutierrez Sanchez <spirosis@hotmail.com>
2024-01-23 20:51:26 -0500

    update

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,9 +2,20 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Post, Comment
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+import requests
+from email.message import EmailMessage
+import smtplib
+from datetime import datetime, timedelta
+import ssl
+import uuid
+from urllib.parse import unquote, quote
+import secrets
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, decode_token
+import os
+from dotenv import load_dotenv
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +31,19 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/users', methods=['GET'])
+def getUsers():
+    users = User.query.all()
+    allUsers = list(map(lambda x: x.serialize(), users))
+
+    return jsonify(allUsers), 200
+
+@api.route('/posts', methods=['GET'])
+@jwt_required()
+def getPosts():
+    current_user_id = get_jwt_identity() 
+    posts = Post.query.filter_by(user_id = current_user_id)
+    allPosts = list(map(lambda x: x.serialize(), posts))
+
+    return jsonify(allPosts), 200
